4823e4e08f2d1c1b1e749f61b3d77ae35aa0103b
benjithompson <mail@benjithompson.com>
2019-11-19 21:32:58 -0600

    Add project files.

---
+++ DexSSL/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DexSSL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            preloadServerConfig();
+            preloadAgentConfig();
+            InitializeComponent();
+        }
+
+        void preloadServerConfig()
+        {
+            if (File.Exists(@"C:\Program Files(x86)\TRICENTIS\Tosca Server\DEXServer\web.config"))
+            {
+                tbServerConfig.Text = @"C:\Program Files(x86)\TRICENTIS\Tosca Server\DEXServer\web.config";
+            }
+        }
+
+        void preloadAgentConfig()
+        {
+            if (File.Exists(@"C:\Program Files(x86)\TRICENTIS\Tosca Testsuite\Distribution Agent\ToscaDistributionAgent.exe.config"))
+            {
+                tbAgentConfig.Text =
+                    @"C:\Program Files(x86)\TRICENTIS\Tosca Testsuite\Distribution Agent\ToscaDistributionAgent.exe.config";
+            }
+        }
+        private void ButtonOpenServerConfig_OnClick(object sender, RoutedEventArgs e)
+        {
+           var path = @"C:\Program Files (x86)\TRICENTIS\Tosca Server\DEXServer";
+           OpenFileDialog dlg = new OpenFileDialog { InitialDirectory = (Directory.Exists(path)) ? path : @"C:\" };
+
+            if (dlg.ShowDialog() != DialogResult)
+            {
+                var filename = dlg.FileName;
+                tbServerConfig.Text = filename;
+            }
+        }
+
+        private void ButtonOpenAgentConfig_OnClick(object sender, RoutedEventArgs e)
+        {
+            var path = @"C:\Program Files(x86)\TRICENTIS\Tosca Server\DEXServer";
+            OpenFileDialog dlg = new OpenFileDialog { InitialDirectory = (Directory.Exists(path)) ? path : @"C:\Program Files (x86)\TRICENTIS" };
+
+            if (dlg.ShowDialog() != DialogResult)
+            {
+                var filename = dlg.FileName;
+                tbAgentConfig.Text = filename;
+            }
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DexSSL/ToscaConfig.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Text;
+
+namespace DexSSL
+{
+    public class ToscaConfig : IDataErrorInfo
+    {
+
+
+        public string ServerConfig { get; set; }
+        public string AgentConfig { get; set; }
+        public string DexServerHostIP { get; set; }
+
+        public string DexServerPort
+        {
+            get { return DexServerPort; }
+            set { DexServerPort = DexServerHostIP.StartsWith("https") ? "443" : "80"; }
+        }
+
+        public string this[string name]{
+            get { 
+                string result = String.Empty;
+                if (name == "ServerConfig")
+                {
+                    if (this.ServerConfig == "")
+                    {
+                        result = "Server web.config path must not be empty";
+                    }
+                }
+                if (name == "AgentConfig")
+                {
+                    if (this.AgentConfig == "")
+                    {
+                        result = "Agent web.config path must not be empty";
+                    }
+                }
+                if (name == "DexServerHostIP")
+                {
+                    if (this.DexServerHostIP == "")
+                    {
+                        result = "Host/IP must not be empty";
+                    }
+                }
+                return result;
+            }
+        }
+
+
+        public string Error => null;
+    }
+}
